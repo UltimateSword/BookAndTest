3469e7c59c6a7f1bb435cdb5dcd4cc8e1acedeff
liuchengpu <liuchengpu@rong360.com>
2019-11-22 13:35:52 +0800

    add huffman encode and cpp linked list

--- BasicDataStructure/BinaryTree/pyTree.py
+++ BasicDataStructure/BinaryTree/pyTree.py
@@ -1,4 +1,98 @@
+from collections import Counter, deque
+import heapq
+
 class Node(object):
     def __init__(self, val):
         self.val = val
         self.left = self.right = None
+
+
+class Travel(object):
+    def __init__(self):
+        self.res = []
+
+    def pre_order(self, root):
+        if root:
+            self.res.append(root.val)
+            self.pre_order(root.left)
+            self.pre_order(root.right)
+
+    def mid_order(self, root):
+        if root:
+            self.mid_order(root.left)
+            self.res.append(root.val)
+            self.mid_order(root.right)
+
+    def post_order(self, root):
+        if root:
+            self.post_order(root.right)
+            self.post_order(root.left)
+            self.res.append(root.val)
+
+# binary search tree: bst,二叉查找树,中序遍历得到有序数组的树,即右节点值大约左节点的树
+# 优点是将有序数组查找的方便性和链表插入的方便性结合了起来
+
+
+class HuffNode(object):
+
+    def __init__(self, val, char=''):
+        self.val = val
+        self.char = char
+        self.coding = ''
+        self.left = self.right = None
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+    def __gt__(self, other):
+        return self.val > other.val
+
+
+class HuffmanCompression(object):
+    def __init__(self, string):
+        self.string = string
+        counter = Counter(string)
+        heap = []
+        for char, cnt in counter.items():
+            heapq.heappush(heap, HuffNode(cnt, char))
+
+        while len(heap) != 1:
+            left = heapq.heappop(heap)
+            right = heapq.heappop(heap)
+            new_node = HuffNode(left.val+right.val)
+            new_node.left, new_node.right = left, right
+            heapq.heappush(heap, new_node)
+
+        self.root = heap[0]
+        self.s2b = {}
+        self.bfs_encode(self.root, self.s2b)
+
+    @staticmethod
+    def bfs_encode(root, s2b):
+        queue = deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            if node.char:
+                s2b[node.char] = node.coding
+                continue
+            if node.left:
+                node.left.coding = node.coding + '0'
+                queue.append(node.left)
+            if node.right:
+                node.right.coding = node.coding + '1'
+                queue.append(node.right)
+
+    def compress(self):
+        bits = ''
+        for char in self.string:
+            bits += self.s2b[char]
+        return bits
+
+
+if __name__ == '__main__':
+    s = HuffmanCompression('123aaaaa' + 'a'*1000)
+    print(s.compress())
--- BasicDataStructure/LinkedList/linked.cpp
+++ BasicDataStructure/LinkedList/linked.cpp
@@ -1,9 +1,151 @@
 #include <iostream>
+
 using namespace std;
 
-struct ListNode{
-    int val;
-    ListNode *next;
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int da = 0, Node *p = nullptr) {  // 构造函数
+        this.data = da;
+        this.next = p;
+    }
 };
 
+class List {
+private:
+    Node *head, *tail;
+public:
+    List() { head = tail = nullptr; };  // 构造函数
 
+    ~List() {
+        delete head;
+        delete tail;
+    };
+
+    void print();
+
+    void Insert(int da = 0);
+
+    void Delete(int da = 0);
+
+    void Search(int da = 0);
+
+    int getValueAt(int position);
+
+    void setValueAt(int position, int da);
+};
+
+int List::getValueAt(int position) {
+    Node *p = head;
+    if (p == nullptr) {
+        cout << "The List is Empty!" << endl;
+    } else {
+        int posi = 0;
+        while (p != nullptr && posi != position) {
+            posi++;
+            p = p.next;
+        }
+        if (p == nullptr) {
+            cout << "There is no value of this position in this List!" << endl;
+        } else {
+            cout << "In this Position,the value is" << p.data << endl;
+        }
+    }
+    return p.data;
+}
+
+void List::setValueAt(int position, int da) {
+    Node *p = head;
+    if (p == nullptr) {
+        cout << "The List is Empty!" << endl;
+    } else {
+        int posi = 0;
+        while (p != nullptr && posi != position) {
+            posi++;
+            p = p.next;
+        }
+        if (p == nullptr) {
+            cout << "There is No Position in this List!" << endl;
+        } else {
+            p.data = da;
+            cout << "The Value in this position has been Updated!" << endl;
+        }
+    }
+}
+
+void List::Search(int da) {
+
+    Node *p = head;
+    if (p == nullptr) {
+        cout << "Sorry, The List is Empty!" << endl;
+        return;
+    }
+    int count = 0;
+    while (p != nullptr && p.data != da) {
+        p = p.next;
+        count++;
+    }
+    cout << "the value you want to search is at position %d" << count << endl;
+}
+
+void List::Delete(int da) {
+    Node *p = head, *q = head;
+    if (p == nullptr) {
+        cout << "Sorry, The List is Empty!" << endl;
+        return;
+    }
+    while (p != nullptr && p.data != da) {
+        q = p;
+        p = p.next;
+    }
+    q.next = p.next;
+    cout << "The Deletion Operation had been finished!" << endl;
+}
+
+void List::Insert(int da) {
+    if (head == nullptr) {
+        head = tail = new Node(da);
+        head.next = nullptr;
+        tail.next = nullptr;
+    } else {
+        Node *p = new Node(da);
+        tail.next = p;
+        tail = p;
+        tail.next = nullptr;
+    }
+
+}
+
+void List::print() {
+    Node *p = head;
+    while (p != nullptr) {
+        cout << p.data << " ";
+        p = p.next;
+    }
+    cout << endl;
+}
+
+int main() {
+    cout << "Hello World!" << endl;
+    List l1;
+    l1.Insert(1);
+    l1.Insert(2);
+    l1.Insert(3);
+    l1.Insert(4);
+    l1.Insert(5);
+    l1.Insert(6);
+    l1.Insert(7);
+    l1.print();
+    l1.Search(4);
+    l1.Delete(6);
+    l1.print();
+    l1.getValueAt(3);
+    l1.setValueAt(3, 9);
+    l1.print();
+    cout << "The End!" << endl;
+    Node *a = new Node(1);
+    cout << a << " 值:" << a.data;
+    return 0;
+}
