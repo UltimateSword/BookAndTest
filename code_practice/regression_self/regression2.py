2dd8670971dfa34a1096b83c4c9462a16253380b
liuchengpu <liuchengpu@rong360.com>
2019-12-18 19:52:27 +0800

    bug fix

--- code_practice/regression_self/regression2.py
+++ code_practice/regression_self/regression2.py
@@ -1,6 +1,11 @@
 def is_match(s, p) -> bool:
     if not p:
         return not s
+    if not s:
+        for i in p:
+            if i != "*":
+                return False
+        return True
     first = p[0] in {s[0], '?', '*'}
     if p[0] == '*' and len(p) < 2:
         return first
@@ -15,5 +20,10 @@ def is_match(s, p) -> bool:
         return False
 
 
+def dp_match(s: str, p: str) -> bool:
+    dp = [[False for _ in p] for _ in s]
+
+
+
 if __name__ == '__main__':
-    print(is_match("abc", "*abc"))
+    print(is_match("mississippi", "m??*ss*?i*pi"))
